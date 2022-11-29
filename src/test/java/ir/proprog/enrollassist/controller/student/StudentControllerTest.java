bda6f8595999de0c4dd06e521cc7d3454c087039
Mahdi <mahdi57.kamali1@gmail.com>
2022-11-29 15:59:00 +0330

    Add test method one

--- src/test/java/ir/proprog/enrollassist/controller/student/StudentControllerTest.java
+++ src/test/java/ir/proprog/enrollassist/controller/student/StudentControllerTest.java
@@ -7,6 +7,7 @@ import ir.proprog.enrollassist.domain.student.StudentNumber;
 import ir.proprog.enrollassist.repository.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -19,7 +20,7 @@ import java.util.Optional;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class StudentControllerTest {
+class StudentControllerTest {
     private StudentController studentController;
     private StudentRepository studentRepo;
     private CourseRepository courseRepo;
@@ -38,27 +39,28 @@ public class StudentControllerTest {
         sectionRepo = Mockito.mock(SectionRepository.class);
         userRepo = Mockito.mock(UserRepository.class);
 
-        students = new ArrayList<>();
         Student s1 = new Student("810198001", GraduateLevel.Undergraduate.toString());
         Student s2 = new Student("810100002", GraduateLevel.Undergraduate.toString());
-        Student s3 = new Student("810100032", GraduateLevel.Undergraduate.toString());
+        Student s3 = new Student("810100003", GraduateLevel.Undergraduate.toString());
+
+        students = new ArrayList<>();
         students.add(s1);
         students.add(s2);
         students.add(s3);
+
         studentViews = new ArrayList<>();
         studentViews.add(new StudentView(s1));
         studentViews.add(new StudentView(s2));
         studentViews.add(new StudentView(s3));
-        studentViews.get(0).setUserId("1");
 
         studentController = new StudentController(studentRepo, courseRepo, sectionRepo, enrollmentListRepo, userRepo);
     }
 
     @Test
     public void testOneReturnsOneStudent() {
-        StudentNumber stdNo = new StudentNumber("810198001");
-        when(studentRepo.findByStudentNumber(stdNo)).thenReturn(Optional.ofNullable(students.get(0)));
-        Assertions.assertEquals(studentViews.get(0), studentController.one(stdNo.toString()));
+        Student student = students.get(1);
+        StudentView studentView = studentViews.get(1);
+        when(studentRepo.findByStudentNumber(student.getStudentNumber())).thenReturn(Optional.of(student));
+        org.assertj.core.api.Assertions.assertThat(studentView).usingRecursiveComparison().isEqualTo(studentController.one(student.getStudentNumber().getNumber()));
     }
 }
-
